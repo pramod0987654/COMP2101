891c28b6df6632956bfa1836b83c761182392916
pramod0987654 <pramodpandey2080@gmail.com>
2023-01-31 22:11:10 -0500

    Assignment Completed

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,6 +2,5 @@
 echo "FQDN: $(hostname)"
 echo "Host Information:"
 hostnamectl
-echo "IP Address: $(ip addr | grep -w inet | awk '{print$2}' | tail -n 1
-)"
+echo "IP Address: $(ip addr | grep -w inet | awk '{print$2}' | tail -n 1)"
 df -h /run
